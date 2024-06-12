b36d1fd459c77be4dc2b6054e6703a18e9fd3877
Leonardo Hernandez <leo.qro@comunidad.unam.mx>
2024-06-12 13:39:48 -0600

    Remove LD regions for PCA

--- removing_LD_for_pca.sh
+++ removing_LD_for_pca.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
-#this script is a step in the process of automatizing a GWAS
+
 #this script will remove LD regions from the data to be used in PCA
-#This script must be run after QC and removal of High LD and other complex regions
+#This script must be run after the removal of High LD and other complex regions from the genomic structure
 
 
 #we must first define the variables that will be used in this script
 #we also have to create a new folder to store the results of this step
-
 mkdir ${outdirectory}${todays_date}_QC_for_pca/${todays_date}_LD_pruning_for_pca
+
+#Output file from the QC step perfomed in QC_for_PCA.sh
 input_for_LD_pruning=${outdirectory}${todays_date}_QC_for_pca/${todays_date}_generalQC/${todays_date}_output_from_QC
+
+#Where the output from this step will be stored
 output_for_LD_pruning=${outdirectory}${todays_date}_QC_for_pca/${todays_date}_LD_pruning_for_pca/${todays_date}_output_from_LD_pruning
+
+#Output file from the first plink step below
 set_of_snps_to_prune=${outdirectory}${todays_date}_QC_for_pca/${todays_date}_LD_pruning_for_pca/${todays_date}_set_of_snps_to_prune
 
 #now we will use plink to remove LD regions from the data
