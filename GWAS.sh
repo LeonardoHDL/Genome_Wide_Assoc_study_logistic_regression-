7309fc7ed7408b24d8cc0479c55d7ad6e2e0498f
Leonardo Hernandez <leo.qro@comunidad.unam.mx>
2024-06-12 13:27:23 -0600

    Master script

--- GWAS.sh
+++ GWAS.sh
@@ -8,6 +8,9 @@
 #in the outdirectory new folders will be created to place the results of each step of this study
 outdirectory='/mnt/Guanina/cvan/data/Keloids_F2/Analysis/leo_analysis/20230501_playing_mafs/maf_0.01_pca_and_assoc/'
 
+
+
+path_to_extrafiles='/mnt/Guanina/cvan/data/Keloids_F2/Analysis/leo_analysis/20230227_PCA_results_and_extrafiles/'
 #it is import to note that this directory is where the extrafiles are stored, these extrafiles are:
 #covar file, pheno file, clin file, plott files to obtain Manhattan, QQ and PCA
 #covar and pheno file are the same as in the PCA step, but you must place them there if you skip PCA step
@@ -17,17 +20,20 @@ outdirectory='/mnt/Guanina/cvan/data/Keloids_F2/Analysis/leo_analysis/20230501_p
 #in this directory you must place the results of a previous PCA (eigenvec) if you want to skip PCA step 
 #in this study. You must also have a 
 #these must be named as follows: eigenvec file must be named as: covarfile.txt, and pheno file must be named as: pheno.txt
-path_to_extrafiles='/mnt/Guanina/cvan/data/Keloids_F2/Analysis/leo_analysis/20230227_PCA_results_and_extrafiles/'
 
 #
 export outdirectory
 export path_to_extrafiles
-#Directory where the raw data are stored
-#data must be in .b format.
+
+
 input_file=/mnt/Guanina/cvan/data/Keloids_F2/WES/Plink/UCHC_Freeze_Two.GL.splitmulti
 export input_file
+#Directory where the raw data are stored
+#data must be in .b format.
 
-#THis will help us to automatize a little bit more this study
+
+#Following steps are optional, if not specified then they'll be set to default values
+#This will help us to automatize a little bit more this study
 #not a necessary step, if some arguments are not specified then they'll be set to default
 #values
 while getopts i:x:g:m:a:h:n:r:o:p flag
@@ -65,7 +71,7 @@ echo "directory to extrafiles: ${path_to_extrafiles}"
 echo "input file: ${input_file}"
 
 ##comment if you dont want that step to be done
-#only removing_complexes.sh, pca.sh, and LD prunning can be commented, the others are necessary
+#only removing_complexes.sh, QC_for_PCA.sh, removing_LD_for_pca.sh, and pca.sh can be commented (or skipped), the other steps are necessary
 /mnt/Guanina/cvan/data/Keloids_F2/Analysis/leo_analysis/20230310_automatizing_scripts_and_trials/removing_complexes.sh #this script remove regions that may affect PCA values and the association study
 /mnt/Guanina/cvan/data/Keloids_F2/Analysis/leo_analysis/20230310_automatizing_scripts_and_trials/QC_for_PCA.sh #this script is meant to do a QC for the PCA step
 /mnt/Guanina/cvan/data/Keloids_F2/Analysis/leo_analysis/20230310_automatizing_scripts_and_trials/removing_LD_for_pca.sh #this script is meant to remove LD regions for the PCA step
